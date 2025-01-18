59a976f6fda9fc7c04e68b403c22a2e56adbc240
park kang lim <gr1993@naver.com>
2025-01-18 14:05:59 +0900

    FileService 구현

---
+++ mall/src/main/java/com/park/mall/service/file/FileService.java
@@ -0,0 +1,84 @@
+package com.park.mall.service.file;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class FileService {
+
+    @Value("${file.upload.dir}")
+    private String fileUploadDir;
+
+    private final SimpleDateFormat dtFormat = new SimpleDateFormat("yyyyMMdd");
+
+    /**
+     * 임시 업로드 처리
+     * @return 생성된 파일명
+     */
+    public String uploadTemp(MultipartFile file) {
+        try {
+            if(!file.isEmpty()) {
+                File tempDir = new File(getTempPath());
+                if (!tempDir.exists()) {
+                    tempDir.mkdir();
+                }
+
+                String storeFileName = createStoreFileName(file.getOriginalFilename());
+                String fullPath = getTempPath() + storeFileName;
+                file.transferTo(new File(fullPath));
+                return storeFileName;
+            } else {
+                return null;
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException("파일 작업 중 문제 발생", ex);
+        }
+    }
+
+    /**
+     * 임시 업로드에서 실제 경로로 업로드 처리
+     * @return 업로드 경로
+     */
+    public String uploadFromTemp(String storeFileName) throws IOException {
+        File file = new File(getTempPath() + storeFileName);
+        if (!file.exists()) {
+            return null;
+        }
+
+        String todayStr = dtFormat.format(new Date());
+        String realDirStr = fileUploadDir + "/" + todayStr;
+        File realDir = new File(realDirStr);
+        if (!realDir.exists()) {
+            realDir.mkdir();
+        }
+
+        String fullPath = realDirStr + "/" + storeFileName;
+        File realFile = new File(fullPath);
+        Files.copy(file.toPath(), realFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        return fullPath.replace(fileUploadDir, "");
+    }
+
+    public String getTempPath() {
+        return fileUploadDir + "/temp/";
+    }
+
+    private String createStoreFileName(String originalFilename) {
+        String uuid = UUID.randomUUID().toString();
+        String ext = extractExt(originalFilename);
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return  originalFilename.substring(pos + 1);
+    }
+}
---
+++ mall/src/test/java/com/park/mall/service/FileServiceTest.java
@@ -0,0 +1,68 @@
+package com.park.mall.service;
+
+import com.park.mall.service.file.FileService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.File;
+import java.io.InputStream;
+
+@SpringBootTest
+public class FileServiceTest {
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${file.upload.dir}")
+    private String fileUploadDir;
+
+    InputStream inputStream;
+
+    @BeforeEach
+    public void setup() {
+        inputStream = getClass().getResourceAsStream("/static/img/test.jpg");
+    }
+
+    @Test
+    void fileTempUpload() throws Exception {
+        //given
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file", // 요청 파라미터 이름
+                "test.jpg",   // 원래 파일 이름
+                "image/jpeg", // MIME 타입
+                inputStream
+        );
+
+        //when
+        String fileName = fileService.uploadTemp(multipartFile);
+
+        //then
+        File file = new File(fileService.getTempPath() + fileName);
+        Assertions.assertTrue(file.exists());
+    }
+
+    @Test
+    void uploadFromTemp() throws Exception {
+        //given
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file", // 요청 파라미터 이름
+                "test.jpg",   // 원래 파일 이름
+                "image/jpeg", // MIME 타입
+                inputStream
+        );
+        String fileName = fileService.uploadTemp(multipartFile);
+
+        //when
+        String uploadPath = fileService.uploadFromTemp(fileName);
+
+        //then
+        File file = new File(fileUploadDir + "/" + uploadPath);
+        Assertions.assertTrue(file.exists());
+    }
+
+}
